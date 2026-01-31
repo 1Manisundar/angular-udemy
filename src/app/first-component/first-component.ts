c74e85afe288f494ca281a58be4c2f155dfe5c62
1Manisundar <chsundar143@gmail.com>
2026-01-31 19:11:36 +0530

    event binding + counter example

--- src/app/first-component/first-component.ts
+++ src/app/first-component/first-component.ts
@@ -8,14 +8,36 @@ import { SecondComponent } from '../second-component/second-component';
   styleUrl: './first-component.css',
 })
 export class FirstComponent {
-   who = "Mani";
+  who = "Mani";
 
-   a= 5;
-   b =10;
-   value = this.a + this.b
+  a = 5;
+  b = 10;
+  value = this.a + this.b
 
-   propertyValue : string = "Hi this is me";
-   disable : boolean = false;
+  propertyValue: string = "Hi this is me";
+  disable: boolean = false;
 
-   image = 'https://picsum.photos/id/237/200/300'
+  image = 'https://picsum.photos/id/237/200/300'
+
+  event() {
+    alert("This is event binding");
+  }
+
+  event1(msg: any) {
+    alert(msg);
+  }
+
+  currentMessage: string = "";
+
+  onTyping(event: Event) {
+    // We cast the event target to an HTMLInputElement to get the value
+    const element = event.target as HTMLInputElement;
+    this.currentMessage = element.value;
+    console.log("User typed:", element.value);
+  }
+
+  count:number =0;
+  counter(type:string){
+    type === 'incre' ? this.count++ : this.count--;
+  }
 }
