cb5eef015cb8e656d42d4671f1efe3410a28add7
1Manisundar <chsundar143@gmail.com>
2026-02-02 22:07:28 +0530

    Keybaord events, Two way binding

--- src/app/first-component/first-component.ts
+++ src/app/first-component/first-component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SecondComponent } from '../second-component/second-component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-first-component',
-  imports: [SecondComponent],
+  imports: [SecondComponent, FormsModule],
   templateUrl: './first-component.html',
   styleUrl: './first-component.css',
 })
@@ -36,8 +37,68 @@ export class FirstComponent {
     console.log("User typed:", element.value);
   }
 
+
+  /** Counter Example */
   count:number =0;
+
   counter(type:string){
+    // if (type === 'incre'){
+    //   this.count++;
+    // }else {
+    //   this.count--;
+    // }
+
     type === 'incre' ? this.count++ : this.count--;
   }
+
+
+  /** All other events */
+  dbl = 0;
+  
+  dbclick(){
+    this.dbl++;
+  }
+
+  /** $event */
+  keyup(e:any){
+    console.log(e.target.value);
+  }
+
+  keydown(e: KeyboardEvent){
+    if(e.shiftKey && e.key=== 'Y'){
+      console.log("shift+y clicked");
+
+    }
+  }
+
+  keycount = 0;
+  keyCount(e:KeyboardEvent){
+    if(e.key ==="ArrowUp"){
+    this.keycount++
+    }else if(e.key === "ArrowDown"){
+      this.keycount--;
+    }
+  }
+
+  /** Two way binding */
+  staticBind  = "this is static binding";
+  dynamicBind = ""; 
+
+
+  /** Task */
+  inputVal = '';
+  dynamicVal = '';
+
+  show(){
+    this.dynamicVal = this.inputVal;
+  }
+
+  /**cart task */
+
+  quantity = 1;
+
+  itemPrice =100;
+
+  get cartTotal() {return this.quantity * this.itemPrice;} 
+
 }
